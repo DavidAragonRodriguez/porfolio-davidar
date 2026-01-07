8eb62cc0bc5a0e18e7c08d5a58810ffc30f6e232
DavidAragonRodriguez <David.Aragon1@alu.uclm.es>
2026-01-07 13:27:21 +0100

    feat: añadida sección de inicio completa con foto y cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()], 
+  
+  // Configuración de despliegue
+  site: 'https://DavidAragonRodriguez.github.io',
+  base: '/porfolio-davidar',
 });
